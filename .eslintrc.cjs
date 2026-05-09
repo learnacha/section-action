9783d26678a1caa3191bfa39257cd6ea517cb73c
Sri Hari Acha <learnacha@gmail.com>
2026-05-09 16:14:35 +0400

    add eslint config and lint script

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  settings: { react: { version: '18.2' } },
+  rules: {
+    'react/prop-types': 'off',
+  },
+  overrides: [
+    {
+      files: ['src/**/*.test.{js,jsx}', 'src/test/**/*.{js,jsx}'],
+      env: { node: true },
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        vi: 'readonly',
+      },
+    },
+  ],
+};
